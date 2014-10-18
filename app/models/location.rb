945fef98fa6f38cadbb4b779768ec8eb9169c4b0
Kevin Keller <flygeneticist@gmail.com>
2014-10-18 14:33:46 -0600

    GeoCoder gem added, map_url built based on Location city/ctry.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,10 @@
 class Location < ActiveRecord::Base
   belongs_to :region
+  before_create :generate_map_link
+
+  def generate_map_link
+    coord = Geocoder.coordinates("#{city}, #{ctry}")
+    self.map_link = "http://google.com/maps/@#{coord[0]},#{coord[1]},13z/"
+  end
+
 end
