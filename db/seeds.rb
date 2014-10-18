f02e4c97639eab64c969743c42d0c2fba5c9d464
Kevin Keller <flygeneticist@gmail.com>
2014-10-18 09:09:48 -0600

    Added more fields to Location model. Dates need debugging still.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Admin.create!(:email => 'test@test.com', :password => 'password', :password_confirmation => 'password')
+Region.create(:name => 'South-East Asia')
+Region.create(:name => 'Central America')
+Location.create(:name => 'North-West Hanoi', :city => 'Hoi An', :ctry => 'VN', 
+  :description => 'A thriving costal city, offers some of the best internet in South-East Asia.', 
+  :map_link => 'https://www.google.co.cr/maps/@21.0226967,105.8369637,13z', :monthly_cost => 1077.0, 
+  :best_flight_month => 'Never', :avg_daily_hrs_sun => 0, :region_id => 1, :avg_temp => 30,
+  # :low_season_start => DateTime.strptime("09/01/2009 17:00", "%m/%d/%Y"), 
+  # :low_season_end => DateTime.strptime("09/01/2009 17:00", "%m/%d/%Y"), 
+  # :high_season_start => DateTime.strptime("09/01/2009 17:00", "%m/%d/%Y"), 
+  # :high_season_end => DateTime.strptime("09/01/2009 17:00", "%m/%d/%Y"), 
+  :avg_internet_up => 1000.2, :avg_internet_down => 23000.45, :avg_cost_per_mb => 5.10)
