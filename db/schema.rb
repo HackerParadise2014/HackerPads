96fa6b8f57906268eb114bceb8b5fb34683ffc55
Kevin Keller <flygeneticist@gmail.com>
2014-10-18 00:33:08 -0600

    Region and Location models setup and relationships created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018052819) do
+ActiveRecord::Schema.define(version: 20141018061924) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,18 @@ ActiveRecord::Schema.define(version: 20141018052819) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "locations", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "region_id"
+  end
+
+  add_index "locations", ["region_id"], name: "index_locations_on_region_id"
+
+  create_table "regions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
