e78e2f7e4ef37450d7239681a7f0a3dd5d9805ad
Rob Cole <robcole@gmail.com>
2014-10-19 02:16:00 -0600

    add validations to locations controller for future search wiring

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,27 @@
 class LocationsController < ApplicationController
+
+  before_filter :validate_dates, only: :search
+
   def index
+  end
+
+  def search
+    render :results
   end
 
   def results
   end
+
+  private
+
+  def validate_dates
+    if params && params[:from] && params[:to]
+      @date_from = Date.strptime(params[:from], "%m/%d/%Y")
+      @date_to = Date.strptime(params[:to], "%m/%d/%Y")
+    else
+      @date_from = Date.today
+      @date_to = Date.today + 1.month
+    end
+  end
+
 end
