f1d9389f0f88b739845dd497699d359640cd0ef3
Kevin Keller <flygeneticist@gmail.com>
2014-10-19 07:26:56 -0600

    Flight model added to hold key data

---
+++ app/admin/internet.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register Internet do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  # setting up strong parameters
+  permit_params :location_id, :ISP_name, :data_type, :down_speed, :up_speed, :cost_per_mb
+
+end
---
+++ db/migrate/20141019131007_create_internets.rb
@@ -0,0 +1,12 @@
+class CreateInternets < ActiveRecord::Migration
+  def change
+    create_table :internets do |t|
+      t.string      :ISP_name
+      t.string      :data_type #mobile vs internet
+      t.integer     :down_speed
+      t.integer     :up_speed
+      t.integer     :cost_per_mb, :default => null
+      t.timestamps
+    end
+  end
+end
